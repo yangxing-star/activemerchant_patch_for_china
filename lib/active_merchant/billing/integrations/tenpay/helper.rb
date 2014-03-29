44b6fc3c3ea2d79b85371dea038d9174d16338cf
杨兴 <xing.yang@taodinet.com>
2014-03-29 12:32:16 +0800

    update

--- lib/active_merchant/billing/integrations/tenpay/helper.rb
+++ lib/active_merchant/billing/integrations/tenpay/helper.rb
@@ -24,10 +24,10 @@ module ActiveMerchant #:nodoc:
           def sign
             add_field('sign',
                       Digest::MD5.hexdigest("attach=#{attach}&body=#{body}" +
-                      "&total_fee=#{amount}&input_charset=#{input_charset}" +
-                      "&notify_url=#{notify_url}&out_trade_no=#{order}%partner=#{account}" + 
+                      "&input_charset=#{input_charset}&notify_url=#{notify_url}" +
+                      "&out_trade_no=#{order}%partner=#{account}" + 
                       "&return_url=#{return_url}&spbill_create_ip=#{spbill_create_ip}" +
-                      "&transaction_id=#{transaction_id}&key=#{KEY}").upcase)
+                      "&total_fee=#{amount}&transaction_id=#{transaction_id}&key=#{KEY}").upcase)
           end
 
         end
