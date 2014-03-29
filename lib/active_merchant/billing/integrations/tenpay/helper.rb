5652a02267024a21962e282f2b69daef87ba6339
杨兴 <xing.yang@taodinet.com>
2014-03-29 12:12:40 +0800

    update gateway

--- lib/active_merchant/billing/integrations/tenpay/helper.rb
+++ lib/active_merchant/billing/integrations/tenpay/helper.rb
@@ -7,20 +7,15 @@ module ActiveMerchant #:nodoc:
         class Helper < ActiveMerchant::Billing::Integrations::Helper
           # Replace with the real mapping
           mapping :account, 'bargainor_id'
-          mapping :amount, 'total_fee'
-
-          mapping :order, 'sp_billno'
-
-          mapping :cmdno, 'cmdno'
+          mapping :amount,  'total_fee'
+          mapping :order,   'out_trade_no'
           mapping :return_url, 'return_url'
           mapping :notify_url, 'notify_url'
-          mapping :description, 'desc'
-          mapping :attach, 'attach'
-          mapping :date, 'date'
-          mapping :currency, 'fee_type'
-          mapping :transaction_id, 'transaction_id'
+          mapping :body, 'body'
           mapping :spbill_create_ip, 'spbill_create_ip'
           mapping :input_charset, 'input_charset'
+          mapping :attach, 'attach'
+          mapping :transaction_id, 'transaction_id'
 
           def initialize(order, account, options = {})
             super
@@ -28,10 +23,11 @@ module ActiveMerchant #:nodoc:
 
           def sign
             add_field('sign',
-                      Digest::MD5.hexdigest("cmdno=#{cmdno}&date=#{date}&bargainor_id=#{account}" +
-                      "&transaction_id=#{transaction_id}&sp_billno=#{order}&total_fee=#{amount}" +
-                      "&fee_type=#{currency}&return_url=#{return_url}&attach=#{attach}" +
-                      "&spbill_create_ip=#{spbill_create_ip}&key=#{KEY}").upcase)
+                      Digest::MD5.hexdigest("attach=#{attach}&bargainor_id=#{account}" +
+                      "&body=#{body}&total_fee=#{amount}&input_charset=#{input_charset}" +
+                      "&notify_url=#{notify_url}&out_trade_no=#{order}" + 
+                      "&return_url=#{return_url}&spbill_create_ip=#{spbill_create_ip}" +
+                      "&transaction_id=#{transaction_id}&key=#{KEY}").upcase)
           end
 
         end
